54d0544c1408a1fc9f223655c6600aff209c18d3
AKSHAY R <akshay.renjith.1@gmail.com>
2024-07-22 02:36:21 +0530

    Created 2 Scripts and Scriptable Objects For Items
    - Created Scripts For Scriptable Objects and Enums
    - Created Scriptable Objects For Different Items, each having properties such as Iteem Name, Max Size, Rarity, Price, Weight and Item Type

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum ItemType
+{
+    Consumables,
+    Materials,
+    Weapons,
+    Treasure,
+    All
+
+}
+
+public enum ItemRarity
+{
+    VeryCommon,
+    Common,
+    Rare,
+    Epic,
+    Legendary
+
+}
